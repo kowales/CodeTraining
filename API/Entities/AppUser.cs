9cbe22842dcc9c668a4a7317ab5763f57c5b0992
ken <ken@kenwales.net>
2022-07-21 16:39:54 -0500

    First commit of App

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+
+
+        
+        public string Username { get; set; }
+
+                
+
+
+
+
+    }
+}
